7ca602da65f4db333a8d807c855b9a3b71fc8719
xavier-castro <xavier_castro@icloud.com>
2023-04-17 21:26:27 -0500

    Add footer columns

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -10,9 +10,29 @@ const Footer = (props: Props) => {
         <div className="mt-16 basis-1/2 sm:mt-0">
           <h4 className="font-bold">Xavier Castro Blog and Portfolio</h4>
           <p className="my-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+            dolor sit amet, qui minim labore adipisicing minim sint cillum sint
+            consectetur cupidatat.
           </p>
           <p>Copyright Xavier Castro All Rights Reserved</p>
+        </div>
+        {/* Second Column */}
+        <div className="mt-16 basis-1/4 sm:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Massa orci senectus</p>
+          <p className="my-5">Massa orci senectus</p>
+        </div>
+        {/* Third Column */}
+        <div className="mt-16 basis-1/4 sm:mt-0">
+          <h4 className="font-bold">Contact</h4>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+            ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+            Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+          </p>
+          <p className="my-5">xavier_castro@icloud.com</p>
         </div>
       </div>
     </footer>
